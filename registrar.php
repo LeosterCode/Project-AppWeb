02d1cb99ced7dee171061ea5fda16d00c2f81586
Leonardo Nunez <leosternunez@gmail.com>
2024-07-23 19:01:44 -0600

    Inlucion de BD

---
+++ registrar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body class="image2">
+    <div class="campo-2">
+        <div class="formulario2">
+            <h1>Registrar</h1>
+            <div class="form">
+                <form action="">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="Nombre">
+                        <label for="floatingInput">Nombre</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Email address</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Password</label>
+                        <?php
+                # Inicia Código de REGISTRAR
+               
+                if (isset($_POST['registrar'])) 
+                {  
+                    $nombre=$_POST['matricula'];
+                    $usuario=$_POST['nombre'];
+                    $password=$_POST['carrera'];
+                    $whatsapp=$_POST['whatsapp'];
+                    
+                    if (!empty($matricula) && !empty($nombre) && !empty($whatsapp))	{
+                        
+                    
+                            $sql=$cnnPDO->prepare("INSERT INTO alumnos
+                            (matricula,nombre, carrera, whatsapp) VALUES (:matricula, :nombre, :carrera, :whatsapp)");
+                        
+                            $sql->bindParam(':matricula',$matricula);
+                            $sql->bindParam(':nombre',$nombre);
+                            $sql->bindParam(':carrera',$carrera);
+                            $sql->bindParam(':whatsapp',$whatsapp);
+                            $sql->execute();
+                            unset($sql);
+                            unset($cnnPDO);
+                            ?>
+                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                                <strong>REGISTRO</strong> Sus datos fueron enviados
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                </div>
+                            <?php   
+                    }  
+                }
+                # Termina Código de REGISTRAR
+                ?>
+                        <div class="d-grid gap-2">
+                            <a class="btn btn-primary" type="button" href="index.html">Registrar</a>
+                            <p>¿Ya Tienes Cuenta? <a class="olv-con" href="index.html">Iniciar Sesion</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
