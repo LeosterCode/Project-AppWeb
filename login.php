1aa19536c53e124875e8d21145ec9fadb96c35e7
Emilio-Chr <czemilio70@gmail.com>
2024-07-29 17:15:50 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="body-login">
+    <div class="container-login">
+        <div class="formulario">
+            <h1 class="titulo-login">Login</h1>
+            <form action="">
+                <input class="input-nombre" type="email" required spellcheck="false">
+                <label class="label-nombre">Email</label>
+                <input class="input-password" type="password" required spellcheck="false">
+                <label class="label-password">Password</label>
+                <a href="#"><p class="olv">¿Olvidaste tu contraseña?</p></a>
+                <button class="boton-login">Login</button>
+                <p class="reg">¿Aún no estás registrado? <a class="buttons" href="registrar.html">Registarte</a></p>
+            </form>
+        </div>
+        <div class="img-login"></div>
+    </div>
+</body>
+</html>
+
