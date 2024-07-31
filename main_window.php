ec31c77413ff82cdf64519757a2ffcd5ba2b1c43
Emilio-Chr <czemilio70@gmail.com>
2024-07-31 12:36:04 -0600

    Update main_window.php

--- main_window.php
+++ main_window.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body class="body-index">
+<body class="body-main_window">
             <nav class="navbar navbar-expand-lg color-bg" data-bs-theme="dark">
                 <div class="container-fluid ">
                 <a class="navbar-brand" href="#" style="color: rgb(255, 255, 255);">Chishop</a>
