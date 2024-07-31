f56ec294c23b09920332bda0fe7b12419e386903
Emilio-Chr <czemilio70@gmail.com>
2024-07-30 22:06:46 -0600

    Add files via upload

--- registrar.php
+++ registrar.php
@@ -1,100 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar</title>
+    <title>Register</title>
     <link rel="stylesheet" href="styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
+<body class="body-registrar">
+    <div class="container-registrar">
+        <div class="img-registrar"></div>
+        <div class="formulario-registrar start">
+            <h1 class="titulo-registrar">Register</h1>
+            <form action="">
 
-<body class="image2">
-    <div class="campo-2">
-        <div class="formulario2">
-            <h1>Registrar</h1>
-            <div class="form">
-                <form action="">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="Matricula">
-                        <label for="floatingInput">Matricula</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="Nombre">
-                        <label for="floatingInput">Nombre</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="correo">
-                        <label for="floatingInput">Correo</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="Carrera">
-                        <label for="floatingInput">Carrera</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="Edificio">
-                        <label for="floatingInput">Edificio</label>
-                    </div>
-                    <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
-                        <?php
-                        # Inicia Código de REGISTRAR
+                <input class="input-matricula-registrar" type="text" required spellcheck="false">
+                <label class="label-matricula-registrar">Matricula</label>
 
-                        if (isset($_POST['registrar'])) {
-                            $matricula = $_POST['matricula'];
-                            $nombre = $_POST['nombre'];
-                            $correo = $_POST['correo'];
-                            $carrera = $_POST['carrera'];
-                            $edificio = $_POST['edificio'];
-                            $password = $_POST['password'];
+                <input class="input-nombre-registrar" type="text" required spellcheck="false">
+                <label class="label-nombre-registrar">Nombre</label>
+    
+                <input class="input-email-registrar" type="email" required spellcheck="false">
+                <label class="label-email-registrar">Email</label>
 
-                            if (!empty($matricula) && !empty($nombre) && !empty($correo) && !empty($carrera) && !empty($carrera) && !empty($edificio) && !empty($password)) {
+                <input class="input-numero-registrar" type="text" required spellcheck="false">
+                <label class="label-numero-registrar">Numero</label>
 
 
+                <select class="input-carrera-registrar">
+                    <option value="opcion1"><p>Carrera</p></option>
+                    <option value="opcion2"><p>Innovación de Negocios y Mercadotecnia</p></option>
+                    <option value="opcion3"><p>Diseño y Gestión de Redes Logísticas</p></option>
+                    <option value="opcion4"><p>Biotecnología</p></option>
+                    <option value="opcion5"><p>Confiabilidad de Planta</p></option>
+                    <option value="opcion6"><p>Desarrollo y Gestión de Software</p></option>
+                    <option value="opcion7"><p>Entornos Virtuales y Negocios Digitales</p></option>
+                    <option value="opcion8"><p>Energías Renovables</p></option>
+                    <option value="opcion9"><p>Mecatrónica</p></option>
+                    <option value="opcion10"><p>Metal Mecánica</p></option>
+                    <option value="opcion11"><p>Nanotecnología</p></option>
+                    <option value="opcion12"><p>Procesos y Operaciones Industriales</p></option>
+                    <option value="opcion13"><p>Redes Inteligentes y Ciberseguridad</p></option>
+                    <option value="opcion14"><p>Seguridad Ambiental Sustentable</p></option>
+                    <option value="opcion15"><p>Maestría en Ingeniería para la Manufactura Inteligente</p></option>
+                </select>
+
+                <label class="label-carrera-registrar">Carrera</label>
+
+                <select class="input-edificio-registrar">
+                    <option value="opcion1"><p>Edificio</p></option>
+                    <option value="opcion2"><p>Edificio 1</p></option>
+                    <option value="opcion3"><p>Edificio 2</p></option>
+                    <option value="opcion4"><p>Edificio 3</p></option>
+                    <option value="opcion5"><p>Edificio 4</p></option>
+                </select>
+
+                <label class="label-edificio-registrar">Edificio</label>
 
 
-                                $sql = $cnnPDO->prepare("INSERT INTO alumnos
-                            (matricula, nombre, correo, carrera, edificio, password) VALUES (:matricula, :nombre, :correo, :carrera, :edificio, :password)");
-
-                                $sql->bindParam(':matricula', $matricula);
-                                $sql->bindParam(':nombre', $nombre);
-                                $sql->bindParam(':correo', $correo);
-                                $sql->bindParam(':carrera', $carrera);
-                                $sql->bindParam(':edificio', $edificio);
-                                $sql->bindParam(':password', $password);
-
-                                $sql->execute();
-                                unset($sql);
-                                unset($cnnPDO);
-                        ?>
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <strong>REGISTRO</strong> Sus datos fueron enviados
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-                            <?php
-                            } else {
-                            ?>
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <strong>Error</strong> Tienes campos vacios
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                </div>
-
-                        <?php
-                            }
-                        }
-                        # Termina Código de REGISTRAR
-                        ?>
-                        <div class="d-grid gap-2">
-                            <a class="btn btn-primary" type="button" href="index.html">Registrar</a>
-                            <p>¿Ya Tienes Cuenta? <a class="olv-con" href="index.html">Iniciar Sesion</a></p>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                <input class="input-password-registrar" type="password" required spellcheck="false">
+                <label class="label-password-registrar">Password</label>
+    
+                <button class="boton-registrar">Register</button>
+                <p class="rega">¿Ya tienes cuenta? <a href="login.html" class="buttons">Login</a></p>
+            </form>  
         </div>
+        
     </div>
 </body>
-
 </html>
