2542174759baba6757afb8139ec1b43b1c91012b
emilio <czemilio70@gmail.com>
2024-08-02 17:49:57 -0600

    nuevos estilos de vender

--- vender.php
+++ vender.php
@@ -1,3 +1,21 @@
+<?php
+require 'db_conexion.php';
+session_start();
+if(isset($_POST['reg_prod'])){
+    $id_product = $_POST['id_product'];
+    $name_product = $_POST['name_product'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock'];
+    $id_category = $_POST['id_category'];
+
+    $insert = $cnnPDO -> prepare('INSERT INTO product(id_product, name_product, description, price, stock, id_category) 
+    VALUES ()');
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,30 +29,40 @@
 </head>
 <body class="body-vender">
     <div class="container-vender">
-        <h1>vender</h1>
+        <h1> VENDER</h1>
         <form action="">
             <div>
-                <label for="formFileLg" class="form-label">Seleccione Una Imagen Del Poducto</label>
-                <input class="form-control form-control-lg" id="formFileLg" type="file">
+                <label for="formFileLg" class="form-label text-bg-dark">Seleccione Una Imagen Del Poducto</label>
+                <input name="image1" class="form-control form-control-lg text-bg-dark" id="formFileLg" type="file">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Titulo</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Titulo Del Producto">
+                <input type="text" class="form-control text-bg-dark" id="exampleFormControlInput1" placeholder="Titulo Del Producto">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escriba una Descripcion"></textarea>
+                <textarea class="form-control text-bg-dark" id="exampleFormControlTextarea1" rows="3" placeholder="Escriba una Descripcion"></textarea>
             </div>
-            <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Stock</label>
+                <input type="number" class="form-control text-bg-dark" id="exampleFormControlInput1" placeholder="Stock">
+            </div>
+            <select class="form-select form-select-lg mb-3 text-bg-dark" aria-label="Large select example">
                 <option selected>Categoria</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
+            <?php
+                $select = $cnnPDO->prepare('SELECT * FROM category');
+                $select -> execute();
+                $count = $select-> rowCount();
+                $colum = $select -> fetchAll();
+                foreach ($colum as $data) {
+                echo '<option value="'.htmlentities($data['id_category']).'">'.htmlentities($data['name_category']).'</option>';
+                }
+?>                
             </select>
 
             <div class="d-grid gap-2 col-6 mx-auto">
-                <button class="btn btn-primary" type="button">Publicar</button>
-                <a class="btn btn-primary" type="button" href="main_window.php">Regresar</a>
+                <button name="reg_prod" class="btn btn-success" type="button">Publicar</button>
+                <a  class="btn btn-success" type="button" href="main_window.php">Regresar</a>
             </div>
         </form>
     </div>
