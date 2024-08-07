dc76f141e02d62216819f541e67462c79897a275
emilio <czemilio70@gmail.com>
2024-08-07 00:46:43 -0600

    styles en mis productos xd

--- my_products.php
+++ my_products.php
@@ -10,10 +10,12 @@ require 'navbar.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <title>My Productos</title>
 </head>
 
-<body>
+<body class="body-productos">
+    <div class="container-products">
+        <h1>Mis Productos</h1>
     <?php
     $select = $cnnPDO->prepare('SELECT * FROM product WHERE student_id = ?');
     $select->execute([$_SESSION['student_id']]);
@@ -22,25 +24,29 @@ require 'navbar.php';
 
     if ($count) {
         try {
-            echo '<div class="container mt-5">';
+            echo '<div class="card-mis-productos container mt-5">';
             echo '   <div class="row">';
             foreach ($column as $data) {
-                echo '       <div class="col-md-3 mb-4">';
-                echo '           <div class="card" style="width: 250px; border:solid 1px black; box-shadow: 0 3px 15px">';
+                echo '       <div>';
+                echo '           <div class="carda" style="width: 100%; border:solid 1px black;">';
                 echo '               <img src="data:image_png;base64,' . base64_encode($data['image_1']) . '" class="card-img-top" alt="...">';
-                echo '               <div class="card-body">';
-                echo '                   <p>id producto:  ' . htmlentities($data["id_product"]) . '</p <br>';
-                echo '                   <p>Matricula:  ' . htmlentities($data["student_id"]) . '</p <br>';
-                echo '                   <p>Nombre:  ' . htmlentities($data["name_product"]) . '</p <br>';
-                echo '                   <p>Descripcion:  ' . htmlentities($data["description"]) . '</p <br>';
-                echo '                   <p>Precio:  ' . htmlentities($data["price"]) . '</p <br>';
-                echo '                   <p>Stock:  ' . htmlentities($data["stock"]) . '</p <br>';
-                echo '                   <p>Categoria:  ' . htmlentities($data["id_category"]) . '</p <br>';
+                echo '               <div class="card-body-products ">';
+                echo '                   <p class="id-producto">id producto:  ' . htmlentities($data["id_product"]) . '</p >';
+                echo '                   <p>Matricula:  ' . htmlentities($data["student_id"]) . '</p >';
+                echo '                   <p>Nombre:  ' . htmlentities($data["name_product"]) . '</p >';
+                echo '                   <p>Descripcion:  ' . htmlentities($data["description"]) . '</p >';
+                echo '                   <p>Precio:  ' . htmlentities($data["price"]) . '</p >';
+                echo '                   <p>Stock:  ' . htmlentities($data["stock"]) . '</p >';
+                echo '                   <p>Categoria:  ' . htmlentities($data["id_category"]) . '</p >';
+                
                 echo '               </div>';
+                echo'';
+                echo '<button type="button" class="boton-productos">Ver Producto</button>';
+                echo '<button type="button" class="boton-productoss">Eliminar </button>';
                 echo '           </div>';
                 echo '       </div>';
             }
-            echo '   </div';
+            echo '</div';
             echo '</div>';
         } catch (PDOException $error) {
             echo 'ERROR EN LA BASE DE DATOS' . $error->getMessage();
@@ -49,7 +55,8 @@ require 'navbar.php';
         echo "No tienes productos registrados";
     }
     ?>
+    </div>
+
 
 </body>
-
 </html>
