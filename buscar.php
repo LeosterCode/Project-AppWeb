08fa0422a397c8e8238f7fbc68159088338ac622
Daniel Aguirre <luisdanielaguirreovalle.ldao@gmail.com>
2024-08-08 21:30:34 -0600

    busqueda arreglada?

--- buscar.php
+++ buscar.php
@@ -1,35 +1,36 @@
 <?php
 require 'navbar.php';
 require 'db_conexion.php';
+// Definir las variables para la conexión al PDO
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_webapp_project";
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+try {
+    // Conectar a la base de datos con PDO
+    $utf8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD, $utf8);
+} catch (PDOException $e) {
+
 }
 
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 
-$sql = "SELECT * FROM product WHERE name_product LIKE ?";
-$stmt = $conn->prepare($sql);
+$sql = "SELECT * FROM product WHERE name_product LIKE :searchTerm";
+$stmt = $pdo->prepare($sql);
 $searchTerm = "%" . $query . "%";
-$stmt->bind_param("s", $searchTerm);
+$stmt->bindParam(':searchTerm', $searchTerm, PDO::PARAM_STR);
 $stmt->execute();
-$result = $stmt->get_result();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo '<div class="container mt-5">';
-if ($result->num_rows > 0) {
+if (count($result) > 0) {
     echo "<h2>Resultados de búsqueda:</h2>";
     echo '<div class="row">';
-    while ($row = $result->fetch_assoc()) {
+    foreach ($result as $row) {
         echo '<div class="col-md-4 mb-4">';
-        echo '   <div class="card h-100" style="border: solid 1px black;">';
+        echo '   <div class="card" style="width: 100%; border: solid 1px black;">';
+        echo  '     <a href="product_detail.php?id_product=' . htmlentities($row["id_product"]) . '" style="text-decoration: none; color: inherit;">';
         echo '          <img src="data:image/png;base64,' . base64_encode($row['image_1']) . '" class="card-img-top" alt="Imagen del producto" style="height: 300px; object-fit: cover;">';
-        echo '          <div class="card-body d-flex flex-column" style="display: flex; flex-direction: column; min-height: 250px;">';
+        echo '          <div class="card-body">';
         echo '              <p class="id-producto">ID producto: ' . htmlentities($row["id_product"]) . '</p>';
         echo '              <p>Matrícula: ' . htmlentities($row["student_id"]) . '</p>';
         echo '              <p>Nombre: ' . htmlentities($row["name_product"]) . '</p>';
@@ -50,6 +51,4 @@ if ($result->num_rows > 0) {
     echo "No se encontraron productos.";
 }
 
-$stmt->close();
-$conn->close();
 ?>
