179f28bbdcf0cb35a3926b20fe2eaee6727cae4a
emilio <czemilio70@gmail.com>
2024-08-10 20:59:07 -0600

    href en my products

--- my_products.php
+++ my_products.php
@@ -44,7 +44,7 @@ require 'navbar.php';
                 echo '                   <p>Stock:  ' . htmlentities($data["stock"]) . '</p >';
                 echo '</div>';
                 echo '<div class="element-5">';
-                echo '<button type="button" class="boton-productos">Ver Producto</button>';
+                echo '<a href="#" type="button" class="boton-productos">Ver Producto</a>';
                 echo '<button type="button" class="boton-productos-editar">Editar </button>';
                 echo '<button type="button" class="boton-productos-eliminar">Eliminar </button>';
                 echo '</div>';
