24e03a536cc8999e5bc0b31b12e08055a15a6850
emilio <czemilio70@gmail.com>
2024-08-14 00:11:16 -0600

    arreglos

--- buscar.php
+++ buscar.php
@@ -12,10 +12,12 @@ require 'navbar.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body class="body-buscar">
 
-    <div class="container-cards-buscar">
+<div class="container-cards-buscar">
+<div class="container-cardss">
 <?php
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 $searchTerm = "%" . $query . "%";
@@ -25,36 +27,36 @@ $search->execute();
 $count = $search->rowCount();
 $result = $search->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<div class="container">';
+
 if ($count) {
-    echo "<h2>Resultados de búsqueda:</h2>";
-    echo '<div class="container-cardss" style="border:solid 1px white">';
+    
+    
     foreach ($result as $row) {
-        echo '<div >';
-        echo '   <div class="cardss" style=" border: solid 1px black;">';
+        echo '<div>';
+        echo '   <div class="cardss"  style="width: 300px;" height: 100%;>';
         echo  '     <a class="col-3" href="window_product.php?slug=' . htmlentities($row["slug_product"]) . '" style="text-decoration: none; color: inherit;">';
-        echo '          <img src="data:image/png;base64,' . base64_encode($row['image_1']) . '" class="card-img-top" alt="Imagen del producto" style="height: 300px; object-fit: cover;">';
+        echo '          <img src="data:image/png;base64,' . base64_encode($row['image_1']) . '" class="card-img-top"  none; alt="Imagen del producto" object-fit: cover;">';
         echo '          <div class="card-body">';
         echo '              <p>Nombre: ' . htmlentities($row["name_product"]) . '</p>';
         echo '              <p>Descripción: ' . htmlentities($row["description"]) . '</p>';
         echo '              <p>Precio: ' . htmlentities($row["price"]) . '</p>';
         echo '              <p>Stock: ' . htmlentities($row["stock"]) . '</p>';
         echo '          </div>';
+        
         echo '   </div>';
         echo '</div>';
     }
-    echo '</div>'; 
-echo '</div>';
-  
+     
+
+
 } else {
     echo "No se encontraron productos.";
 }
 
 ?>
 
-
-    </div>
-
+</div>
+</div>
 
 </body>
 </html>
