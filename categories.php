6c908a25240835399277e99450d98fd5827dc302
emilio <czemilio70@gmail.com>
2024-08-15 12:08:06 -0600

    categorias

--- categories.php
+++ categories.php
@@ -25,9 +25,10 @@ if (isset($_GET['slug'])) {
     <title>My Productos</title>
 </head>
 
-<body class="body-productos">
-    <div class="container-products">
-        <h1>Mis Productos</h1>
+<body class="body-categorias">
+<h1>Categoria</h1>
+    <div class="container-cards">
+        
     <?php
     $select = $cnnPDO->prepare('SELECT * FROM product WHERE slug_category = :slug_category');
     $select->bindParam(':slug_category',$slug_category);
@@ -37,30 +38,23 @@ if (isset($_GET['slug'])) {
 
     if ($count) {
         try {
-            echo '<div class="card-mis-productos container mt-5">';
-            echo '   <div class="row">';
+            
+            echo '   <div class="cards">';
             foreach ($column as $data) {
-                echo '       <div>';
-                echo '           <div class="carda" style="width: 100%; border:solid 1px black;">';
-                echo '               <img src="data:image_png;base64,' . base64_encode($data['image_1']) . '" class="card-img-top" alt="...">';
-                echo '               <div class="card-body-products ">';
-                echo '                   <p class="id-producto">id producto:  ' . htmlentities($data["id_product"]) . '</p >';
-                echo '                   <p>Matricula:  ' . htmlentities($data["student_id"]) . '</p >';
-                echo '                   <p>Nombre:  ' . htmlentities($data["name_product"]) . '</p >';
-                echo '                   <p>Descripcion:  ' . htmlentities($data["description"]) . '</p >';
-                echo '                   <p>Precio:  ' . htmlentities($data["price"]) . '</p >';
-                echo '                   <p>Stock:  ' . htmlentities($data["stock"]) . '</p >';
-                echo '                   <p>Categoria:  ' . htmlentities($data["name_category"]) . '</p >';
+                echo '  <div class="card border-dark mx-3 mb-3" style="max-width: 18rem;">';
+                echo '    <img src="data:image_png;base64,' . base64_encode($data['image_1']) . '" class="card-img-top" alt="...">';
+                echo '    <div class="card-body">';
+                echo '      <h5 class="card-name">' . htmlentities($data['name_product']) . '</h5>';
+    
+                echo '      <p class="card-text">$' . htmlentities($data['price']) . '</p>';
+                echo '              <p>Stock: ' . htmlentities($data["stock"]) . '</p>';
+                echo '    </div>';
+                echo '      <a href="window_product.php?slug=' . htmlentities($data['slug_product']) . ' " >Ver producto</a>';
+                echo '  </div>';
                 
-                echo '               </div>';
-                echo'';
-                echo '<button type="button" class="boton-productos">Ver Producto</button>';
-                echo '<button type="button" class="boton-productoss">Eliminar </button>';
-                echo '           </div>';
-                echo '       </div>';
             }
             echo '</div';
-            echo '</div>';
+            
         } catch (PDOException $error) {
             echo 'ERROR EN LA BASE DE DATOS' . $error->getMessage();
         }
